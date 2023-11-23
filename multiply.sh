26d84b30d57ba207c2b4163d38c50dc872923bd2
Ubuntu <ubuntu@ip-172-31-41-152.ap-south-1.compute.internal>
2023-11-23 12:18:33 +0000

    updating multiplication comments

--- multiply.sh
+++ multiply.sh
@@ -1,1 +1,12 @@
-# bashscripting
+# ! /bin/bash
+
+echo "Enter the first number:"
+read n1
+
+echo "Enter the second number:"
+read n2
+
+
+result=$((n1 * n2))
+
+echo "The total vaulue is :$result"
